5139adc18e90ce48f6027066bcaaa3c8ea5d84af
albana <acelepija@fbk.eu>
2025-10-09 15:53:32 +0200

    chore(agents) prepare prompts structure

--- commands.sh
+++ commands.sh
@@ -4,3 +4,11 @@ curl -fsSL https://ollama.com/install.sh | sh
 #ollama run llama3.2
 ollama pull llama3.2:1b # run llama3.2 in 1b mode since it's small'
 ollama run llama3.2:1b
+
+
+# generate response
+curl http://localhost:11434/api/generate -d '{
+  "model": "llama3.2",
+  "prompt": "Why is the sky blue?",
+  "stream": false
+}'
