cfa78f562fc242604dac9c7e3abe93b0fd276ea9
AlbanaCelepija <acelepija@fbk.eu>
2025-11-24 12:10:37 +0100

    core(agents)rag agents and initial orchestrator

--- commands.sh
+++ commands.sh
@@ -4,6 +4,12 @@ curl -fsSL https://ollama.com/install.sh | sh
 #ollama run llama3.2
 ollama pull llama3.2:1b # run llama3.2 in 1b mode since it's small'
 ollama run llama3.2:1b
+####
+ollama pull mistral:7b
+ollama run mistral:7b
+#### load fine tuned model
+ollama create finetunedmodel -f modelfile
+
 
 
 # generate response
@@ -17,9 +23,10 @@ curl http://localhost:11434/api/generate -d '{
 # LLM observability - Opik setup instructions
 # Clone the Opik repository
 git clone https://github.com/comet-ml/opik.git
-# Navigate to the opik folder
-cd opik
+# Navigate to the opik folder 
+cd opik 
 # Start the Opik platform
 ./opik.sh
 
-
+# MCP server setup instructions
+npx @modelcontextprotocol/inspector uv run mcp_server.py  # src/agents/coding_agents/fairness_agent
